351aa8356e53c31e0374c2a5365c6063686432c3
tonyatpeking <tonyatpeking@gmail.com>
2024-06-11 12:55:39 -0500

    move to webgpu engine

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,6 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, MeshBuilder } from "@babylonjs/core";
+import * as BABYLON from "@babylonjs/core";
 
 export { hellos } from './utils/func'
 
@@ -13,45 +13,94 @@ export function helloAnything(thing: string): string {
 
 
 class App {
-    constructor(canvas?: HTMLCanvasElement | null) {
-        // create the canvas html element and attach it to the webpage
 
-        if (!canvas) {
-            canvas = document.createElement("canvas");
-            canvas.id = "app-canvas";
-            document.body.appendChild(canvas);
+    canvas!: HTMLCanvasElement;
+    appContainer!: HTMLDivElement;
+    engine!: BABYLON.WebGPUEngine;
+    scene!: BABYLON.Scene;
+
+    // constructor should only be called internally
+    // to make an instance of this class, use the static async method App.create()
+    // this is a workaround because the constructor cannot be async
+    protected constructor() {
+    }
+
+    static async create(appContainer: HTMLDivElement): Promise<App | null> {
+        const webGPUSupported = await BABYLON.WebGPUEngine.IsSupportedAsync;
+        if (!webGPUSupported) {
+            appContainer.textContent = 'WebGPU not supported. Please use latest version of Chrome';
+            appContainer.style.color = '#ff3518';
+            appContainer.style.fontSize = '3vw'
+            return null;
         }
+        appContainer.textContent = '';
+
+        let app = new App();
+
+        app.appContainer = appContainer;
+
+        // create canvas
+        app.canvas = document.createElement("canvas");
+        appContainer.appendChild(app.canvas);
+        app.canvas.id = "app-canvas";
+        app.canvas.style.width = "100%";
+        app.canvas.style.height = "100%";
+        app.canvas.style.touchAction = "none";
+        app.canvas.style.margin = "0";
+        app.canvas.style.padding = "0";
+
+
+        // create engine
+        app.engine = new BABYLON.WebGPUEngine(app.canvas);
+        await app.engine.initAsync();
+
+        app.scene = new BABYLON.Scene(app.engine);
+
 
 
         window.addEventListener("resize", function () {
-            engine.resize();
+            app.engine.resize();
         });
 
-        // initialize babylon scene and engine
-        var engine = new Engine(canvas, true);
-        var scene = new Scene(engine);
-
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
-        camera.attachControl(canvas, true);
-        new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-
-        // hide/show the Inspector
         window.addEventListener("keydown", (ev) => {
             // Shift+Ctrl+Alt+I
             if (ev.shiftKey && ev.key === 'I') {
-                if (scene.debugLayer.isVisible()) {
-                    scene.debugLayer.hide();
+                if (app.scene.debugLayer.isVisible()) {
+                    app.scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                    app.scene.debugLayer.show();
                 }
             }
         });
 
-        // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
+        app.createSceneObjects();
+
+        app.engine.runRenderLoop(() => {
+            app.scene.render();
         });
+
+
+        return app;
+    }
+
+    createSceneObjects() {
+
+        // This creates and positions a free camera (non-mesh)
+        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), this.scene);
+        // This targets the camera to scene origin
+        camera.setTarget(BABYLON.Vector3.Zero());
+        // This attaches the camera to the canvas
+        camera.attachControl(this.canvas, true);
+        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this.scene);
+        // Default intensity is 1. Let's dim the light a small amount
+        light.intensity = 0.7;
+        // Our built-in 'sphere' shape. Params: name, options, scene
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, this.scene);
+        // Move the sphere upward 1/2 its height
+        sphere.position.y = 1;
+        // Our built-in 'ground' shape. Params: name, options, scene
+        var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, this.scene);
     }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,10 @@ import { helloAnything, hellos, App } from '../lib/main.ts'
 
 
 //setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-const canvas = document.querySelector<HTMLCanvasElement>('#app-canvas')!
+const container = document.querySelector<HTMLDivElement>('#app-container')!
 console.log(helloAnything('world'))
 console.log(hellos('world'))
 
 //new TriangleScene(canvas);
-new App(canvas);
+let app = await App.create(container);
+console.log(app)
